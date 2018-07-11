bd01d84fc8b3d8e1937f873d1c2b769f525129ee
nyakasv <nyakasv@gmail.com>
2018-07-11 19:14:11 +0200

    Created MemberLists,Details,Resolvers

---
+++ src/app/_resolvers/member-list.resolver.ts
@@ -0,0 +1,20 @@
+import { Observable } from 'rxjs/Observable';
+import { Resolve, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { User } from '../_models/User';
+import { Injectable } from '../../../node_modules/@angular/core';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class MemberListResolver implements Resolve<User[]> {
+    constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) {}
+
+    resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
+        return this.userService.getUsers().catch(error => {
+            this.alertify.error('Problem retreiving data');
+            this.router.navigate(['/home']);
+            return Observable.of(null);
+        });
+    }
+}
---
+++ src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../_models/User';
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from '../../_services/alertify.service';
+import { ActivatedRoute } from '../../../../node_modules/@angular/router';
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+  user: User;
+
+  constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    // use this to get user without member-detail-resolver
+    // this.loadUser();
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+  }
+
+  // route //members/id
+  // use this to get user without member-detail-resolver
+  /*
+  loadUser() {
+    this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
+      this.user = user;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+  */
+}
